5d89d0a3ed4088d8186c883a6db6fdd0a7953d7c
TomRight <zxvc@rambler.ru>
2022-10-02 20:04:30 +0500

    Добавил еще не много стилей, в частности hr чтоб делить разные участки

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,15 @@
     <div class="test">
       Проверка что все работает. Если вы это читаете, значит все работает!
     </div>
+    <hr />
     <!-- для вывода данных из data(), используюется интерполяция или "mustache-style", нужно написать двойные фигурные скобки {{переменная из data()}}  -->
-    <div class="exampleMustache">Count: {{ count }}</div>
+    <div class="exampleMustache">
+      Привязали при помощи интерполяции переменную count: {{ count }}
+    </div>
+    <div class="vbind exampleMustache" :id="header">
+      Делаем динамическую привязку к атрибуту id через v-bind, но в данном
+      случае через : см в инструментах разработчика на это.
+    </div>
   </div>
 </template>
 <script>
@@ -14,6 +21,7 @@ export default {
     // тут можно произвести некоторую обработку данных, перед тем как их вернуть.
     return {
       count: 0, //покскольку возвращается тип Object, то не нужно забывать ставить запятые после объявления поля объекта.
+      tagId: "header",
     };
   },
 };
@@ -24,10 +32,18 @@ export default {
   display: flex;
   flex-direction: column;
 }
-.test{
+.test {
   margin: auto;
 }
-.exampleMustache{
-margin-top: 20px;
+.exampleMustache {
+  margin-top: 20px;
+}
+hr {
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 2px solid green;
+  margin: 1em 0;
+  padding: 0;
 }
 </style>
