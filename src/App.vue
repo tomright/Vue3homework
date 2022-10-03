9cc8e7c9d32297d890cbf1e477d1cf7d5371a385
TomRight <zxvc@rambler.ru>
2022-10-03 12:10:57 +0500

    Логические атрибуты, работа с переменными

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,27 @@
     </div>
     <hr />
     <!-- для вывода данных из data(), используюется интерполяция или "mustache-style", нужно написать двойные фигурные скобки {{переменная из data()}}  -->
-    <div class="exampleMustache">
+    <div class="exampleNote">
       Привязали при помощи интерполяции переменную count: {{ count }}
     </div>
-    <div class="vbind exampleMustache" :id="header">
+    <hr />
+    <div class="vbind exampleNote" :id="tagId">
       Делаем динамическую привязку к атрибуту id через v-bind, но в данном
       случае через : см в инструментах разработчика на это.
     </div>
+    <hr />
+    <div class="exampleNote">
+      <h2>Логические атрибуты</h2>
+      <p>
+        К атрибутам disabled так же можно забиндиться\привязаться, для примера
+        ниже есть 2 кнопки, одна кнопка привязана к значению переменной
+      </p>
+      <button :disabled="isButtonDisabled" @click="alertMsgActivButton">
+        Чтобы активировать нажмите на кнопку активации
+      </button>
+      <button @click="activationButton">{{ buttonActivationName }}</button>
+    </div>
+    <hr />
   </div>
 </template>
 <script>
@@ -22,7 +36,23 @@ export default {
     return {
       count: 0, //покскольку возвращается тип Object, то не нужно забывать ставить запятые после объявления поля объекта.
       tagId: "header",
+      isButtonDisabled: true,
+      buttonActivationName: "Активация кнопки",
     };
+  },
+  methods: {
+    activationButton() {
+      if (this.isButtonDisabled) {
+        this.isButtonDisabled = false;
+        this.buttonActivationName = "Деактивация кнопки";
+      } else {
+        this.isButtonDisabled = true;
+        this.buttonActivationName = "Активация кнопки";
+      }
+    },
+    alertMsgActivButton() {
+      alert("Вы молодец!");
+    },
   },
 };
 </script>
@@ -35,7 +65,7 @@ export default {
 .test {
   margin: auto;
 }
-.exampleMustache {
+.exampleNote {
   margin-top: 20px;
 }
 hr {
